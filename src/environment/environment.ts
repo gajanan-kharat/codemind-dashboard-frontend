e8069d9d6097fe6ed7f9faa5ae353f1a14ca4e1c
Sushama <131261130+SushmaMotekar95@users.noreply.github.com>
2025-01-08 18:43:48 +0530

    added deployed Api Url
    added deployed Api Url

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     // ApiUrl: 'http://localhost:3000/api'
-    ApiUrl: 'https://codemind-backend-node-app.onrender.com/api'
+    // ApiUrl: 'https://codemind-backend-node-app.onrender.com/api'
+    ApiUrl: 'https://codemind-backend.onrender.com/api'
   };
